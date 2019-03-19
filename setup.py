bd37ae83d4252c3f8eb8e9a331550cdaef8e53a0
Shivaan Sook <shivaansook6@gmail.com>
2019-03-19 10:06:01 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='analyse-hackathon',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA hack-a-thon python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/<username>/<package-name>',
+    author='Shivaan Sook',
+    author_email='shivaansook6@gmail.com'
+)
