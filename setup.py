0620807b5772eda8903cb310e3b7939e30f193b6
Shivaan Sook <shivaansook6@gmail.com>
2019-03-19 11:26:10 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='analyse-hackathon',
+    name='recsort',
     version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA hack-a-thon python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
+    url='https://github.com/ShivaanSook/recsort',
     author='Shivaan Sook',
     author_email='shivaansook6@gmail.com'
 )
